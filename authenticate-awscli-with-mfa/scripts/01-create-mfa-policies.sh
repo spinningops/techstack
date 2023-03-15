c5b99aacb125b8de0885dc1f759f55f981584c68
kobiam <kobi700@gmail.com>
2023-03-16 01:19:25 +0200

    update scripts

--- authenticate-awscli-with-mfa/scripts/01-create-mfa-policies.sh
+++ authenticate-awscli-with-mfa/scripts/01-create-mfa-policies.sh
@@ -4,8 +4,5 @@
 # create policy-allow-users-manage-keys
 aws iam create-policy --policy-name policy-allow-users-manage-keys --policy-document file://policy-allow-users-manage-keys.json
 
-# create policy-allow-users-to-self-manage-mfa-device
-aws iam create-policy --policy-name policy-allow-users-to-self-manage-mfa-device --policy-document file://policy-allow-users-to-self-manage-mfa-device.json
-
-# create policy-block-most-access-unless-signed-in-with-mfa
-aws iam create-policy --policy-name policy-block-most-access-unless-signed-in-with-mfa --policy-document file://policy-block-most-access-unless-signed-in-with-mfa.json
+# create policy-users-mfa
+aws iam create-policy --policy-name policy-users-mfa --policy-document file://policy-users-mfa.json
--- authenticate-awscli-with-mfa/scripts/03-attach-mfa-policies-to-mfa-group.sh
+++ authenticate-awscli-with-mfa/scripts/03-attach-mfa-policies-to-mfa-group.sh
@@ -5,10 +5,7 @@ echo "Hi, Please add your AWS account id"
 read ACCOUNT_ID
 
 # attach policy to users-mfa
-aws iam attach-group-policy --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/policy-block-most-access-unless-signed-in-with-mfa --group-name users-mfa
-
-# attach policy to users-mfa
-aws iam attach-group-policy --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/policy-allow-users-to-self-manage-mfa-device --group-name users-mfa
+aws iam attach-group-policy --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/policy-users-mfa --group-name users-mfa
 
 # attach policy to users-mfa
 aws iam attach-group-policy --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/policy-allow-users-manage-keys --group-name users-mfa
